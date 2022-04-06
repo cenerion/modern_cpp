758d267de0de13c7228b3b8fc52d4e95d67ec378
Keiji Shiba <12021270+cenerion@users.noreply.github.com>
2022-04-06 18:25:32 +0200

    using auto where i can and using range based for i place of iterators

--- main.cpp
+++ main.cpp
@@ -35,23 +35,23 @@ bool areaLessThan10(shared_ptr<Shape> s)
 
 void printCollectionElements(const Collection& collection)
 {
-    for(Collection::const_iterator it = collection.begin(); it != collection.end(); ++it)
-        if(*it)
-            (*it)->print();
+    for(auto it : collection)
+        if(it)
+            (it)->print();
 }
 
 void printAreas(const Collection& collection)
 {
-    for(vector<shared_ptr<Shape>>::const_iterator it = collection.begin(); it != collection.end(); ++it)
-        if(*it)
-            cout << (*it)->getArea() << std::endl;
+    for(auto it : collection)
+        if(it)
+            cout << (it)->getArea() << std::endl;
 }
 
 void findFirstShapeMatchingPredicate(const Collection& collection,
                                      bool (*predicate)(shared_ptr<Shape> s),
                                      std::string info)
 {
-    Collection::const_iterator iter = std::find_if(collection.begin(), collection.end(), predicate);
+    const auto iter = std::find_if(collection.begin(), collection.end(), predicate);
     if(*iter != nullptr)
     {
         cout << "First shape matching predicate: " << info << endl;
